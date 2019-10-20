50326bb220c503c4d4594d472d6fd58e2599b4c3
bassio <ahmedbassi@gmail.com>
2019-10-21 02:50:12 +1030

    fix Reference.vue

--- src/templates/Reference.vue
+++ src/templates/Reference.vue
@@ -1,15 +1,19 @@
 <template>
   <Layout>
-    <br>
-    <g-link to="/" class="link">  &larr; Go Back</g-link>
-    <div class="item-name">
-      <h1>Genre: {{$page.genre.name}}</h1>
-      <p class="item-name">id: {{ $page.item.id}}</p>
-      <p class="item-name">Title: {{ $page.item.title}}</p>
-      <p class="item-name">BibTex Entry: {{ $page.genre.bibtex}}</p>
-    </div>
-    <div class="item-content">
-      <p v-html="$page.item" />
+    <PageHeading type="Reference" :title="$page.item.title" />
+    <div class="page-fields container">
+      <div class="field-row row">
+        <div class="field-label two columns">Nahdapedia id: </div>
+        <div class="field-value ten columns">{{ $page.item.id}}</div>
+      </div>
+      <div class="field-row row">
+        <div class="field-label two columns">Title: </div>
+        <div class="field-value ten columns">{{$page.item.title}}</div>
+      </div>
+      <div class="field-row row">
+        <div class="field-label two columns">BibTex Entry: </div>
+        <div class="field-value ten columns">{{$page.item.bibtex}}</div>
+      </div>
     </div>
   </Layout>
 </template>
